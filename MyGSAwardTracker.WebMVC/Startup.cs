2748991e273b441402eb157061c7d1ad8e9023a7
Debbie <debbieforbeck@gmail.com>
2020-01-12 12:02:54 -0500

    Add project files.

---
+++ MyGSAwardTracker.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyGSAwardTracker.WebMVC.Startup))]
+namespace MyGSAwardTracker.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
