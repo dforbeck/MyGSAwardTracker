134bfdef9be8b81221d8f1aa7ffcf1a551187c7b
Debbie <debbieforbeck@gmail.com>
2020-01-13 14:48:56 -0500

    identity models

--- MyGSAwardTracker.Data/IdentityModels.cs
+++ MyGSAwardTracker.Data/IdentityModels.cs
@@ -57,7 +57,10 @@ namespace MyGSAwardTracker.Data
 
     public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
     {
-
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
+        }
     }
 
 }
