6580591362959ee418f0c01df3812ad3732481c9
Debbie <debbieforbeck@gmail.com>
2020-02-06 14:25:07 -0500

    idea list

---
+++ MyGSAwardTracker.Models/IdeaListItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGSAwardTracker.Models
+{
+    public class IdeaListItem
+    {
+        [Display(Name = "Idea ID")]
+        public int IdeaId { get; set; }
+
+        [Display(Name = "Idea Title")]
+        [Required]
+        public string IdeaTitle { get; set; }
+
+        [Display(Name = "Idea Description")]
+        public string IdeaDescription { get; set; }
+
+        [Display(Name = "Date Idea Hatched")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTimeOffset DateIdeaCreated { get; set; }
+
+        [Display(Name = "Date Idea Changed")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTimeOffset? DateIdeaModified { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
